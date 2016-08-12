e5b67128e8ef3e1613cec5c52a794bc4e78a8810
spiddy <dimitris.kapanidis@harbur.io>
2016-08-12 14:41:16 +0300

    default and override values.yaml files are now optional

--- cmd/tide/engine.go
+++ cmd/tide/engine.go
@@ -2,10 +2,10 @@ package main // import "github.com/harbur/tide/cmd/tide"
 
 import (
 	"bytes"
-
 	"k8s.io/helm/cmd/tiller/environment"
 	chartutil "k8s.io/helm/pkg/chartutil"
 	chart "k8s.io/helm/pkg/proto/hapi/chart"
+	"os"
 )
 
 var env = environment.New()
@@ -17,17 +17,25 @@ func readManifest(chartname string) (string, error) {
 		return "", err
 	}
 
-	vals, err := chartutil.ReadValuesFile(chartname + "/values.yaml")
-	debug("Values loaded %v", vals)
-	if err != nil {
-		print_error("Values cannot be loaded: %s", err)
-		return "", err
+	// values.yaml is optional
+	vals := chartutil.Values{}
+	if _, err := os.Stat(chartname + "/values.yaml"); err == nil {
+		vals, err = chartutil.ReadValuesFile(chartname + "/values.yaml")
+		debug("Values loaded %v", vals)
+		if err != nil {
+			print_error("Values cannot be loaded: %s", err)
+			return "", err
+		}
 	}
 
-	overrides, err := chartutil.ReadValuesFile(input_file)
-	if err != nil {
-		print_error("Overrides cannot be loaded: %s", err)
-		return "", err
+	// input_file is optional
+	overrides := chartutil.Values{}
+	if _, err := os.Stat(input_file); err == nil {
+		overrides, err = chartutil.ReadValuesFile(input_file)
+		if err != nil {
+			print_error("Overrides cannot be loaded: %s", err)
+			return "", err
+		}
 	}
 
 	valsYAML, _ := vals.YAML()
